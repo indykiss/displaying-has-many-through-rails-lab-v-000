1d1e2160c3cb90013ea59cda47a15f2ea84ea768
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-03 00:37:59 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controllers.rb
+++ app/controllers/doctors_controllers.rb
@@ -1,11 +1,7 @@
 class DoctorsController < ApplicationController
 
-  def index
-    @doctors = Doctor.all
-  end
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
 
-  def show
-    @doctor = Doctor.find(params[:id])
-  end
-
-end
+end 
